20d6ef302c0a5eacb63eba46596f95531ccf8ffc
Giovanni van der Schelde <gvdschelde@gmail.com>
2024-07-02 08:13:20 +0200

    Add javadoc for Maven Central requirements

--- src/main/java/com/giovds/DependenciesToQueryMapper.java
+++ src/main/java/com/giovds/DependenciesToQueryMapper.java
@@ -5,10 +5,20 @@ import org.apache.maven.model.Dependency;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
+/**
+ * Converts Maven {@link Dependency} to a single Lucene query that can be sent to Maven Central.
+ * This in order to not spam the endpoint with a request per dependency.
+ */
 public class DependenciesToQueryMapper {
+    private DependenciesToQueryMapper() {
+    }
+
     /**
      * Create a <a href="https://lucene.apache.org/core/2_9_4/queryparsersyntax.html">Lucene</a> query from
      * a collection of {@link org.apache.maven.project.MavenProject} {@link Dependency}
+     *
+     * @param dependencies A collection of Maven {@link Dependency} to convert to query params
+     * @return A Lucene query as {@link String} with spaces escaped by '+'
      */
     public static String map(final Collection<Dependency> dependencies) {
         return dependencies
