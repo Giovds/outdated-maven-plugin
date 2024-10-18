f9b5a816f0e4a8f3c5b690a9c494e51b6b94b3bc
Maarten Mulders <maarten@mulders.it>
2024-10-18 17:09:43 +0200

    Sprinkle some JavaDoc on it

--- src/main/java/com/giovds/AverageAgeMojo.java
+++ src/main/java/com/giovds/AverageAgeMojo.java
@@ -18,6 +18,10 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+/**
+ * This plugin goal prints the total and average age of all project dependencies. It excludes dependencies with the
+ * same <pre>groupId</pre> (assuming they are part of the same project, they would skew the outcome).
+ */
 @Mojo(name = "average",
         defaultPhase = LifecyclePhase.VERIFY,
         requiresOnline = true,
@@ -31,7 +35,9 @@ public class AverageAgeMojo extends AbstractMojo {
 
     private final LocalDate today = LocalDate.now();
 
-    // Necessary for Maven
+    /**
+     * Necessary for Maven as there already is an all-arguments constructor for testing purposes.
+     */
     public AverageAgeMojo() {
     }
 
